12cecca32f25e6d1030682a6879b9163cf5ee91e
V3tar <106648586+V3tar@users.noreply.github.com>
2022-06-01 23:12:57 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let btns = document.querySelectorAll('.btn');
+let btn_container = document.querySelector('.btn-container');
+btn_container.addEventListener('click',(e) => {
+    e.preventDefault();
+    btns.forEach((btn) => {
+        btn.classList.remove('orange-active');
+        e.target.classList.add('orange-active');
+    });
+});
+let form = document.querySelector('form');
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    //choose the modal section
+    let modal = document.querySelector('.modal');
+    modal.showModal();
+    //hide the form
+    form.classList.add('hide');
+    //select the h4 in the dialog
+    let h3 = document.querySelector('h3');
+    h3.textContent = `You selected  out of 5`
+});
