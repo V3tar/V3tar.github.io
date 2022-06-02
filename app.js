91c273a5169feb4178d8c68188edffa2bfb56db4
V3tar <106648586+V3tar@users.noreply.github.com>
2022-06-02 16:36:15 +0800

    Better Solutions
    I added further styles and better java functionality.

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-let btns = document.querySelectorAll('.btn');
-let btn_container = document.querySelector('.btn-container');
-btn_container.addEventListener('click',(e) => {
-    e.preventDefault();
-    btns.forEach((btn) => {
-        btn.classList.remove('orange-active');
-        e.target.classList.add('orange-active');
-    });
-});
 let form = document.querySelector('form');
-form.addEventListener('submit',(e) => {
+let rating_btns = document.querySelectorAll('.btn');
+form.addEventListener('submit',(e) =>{
     e.preventDefault();
-    //choose the modal section
-    let modal = document.querySelector('.modal');
-    modal.showModal();
     //hide the form
     form.classList.add('hide');
-    btns.forEach((btn) => {
-        let btn_text = btn.textContent; 
-    });
-     //select the h4 in the dialog
-    let h3 = document.querySelector('h3');
-    h3.textContent = `You selected ${btn_text} out of 5`
+    //show the dialog
+    let modal = document.querySelector('dialog');
+    modal.showModal();
 });
+
+ //add event listener to the rating btns
+rating_btns.forEach((rating_btn) => {
+    rating_btn.addEventListener('click',(e) => {
+        e.preventDefault();
+        let rating = e.target.textContent;
+        let h3 = document.querySelector('h3');
+        h3.textContent = `You have selected ${rating} out of 5`;
+    });
+});
+
+
+
